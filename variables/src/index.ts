a0b852766755f66a4113ee788a221a8b79de8d68
MahdiDelnavazi <delnavazi.mahdi@gmail.com>
2022-09-19 15:37:25 +0430

    init

---
+++ variables/src/index.ts
@@ -0,0 +1,18 @@
+let sale: number = 123_456_789; // number
+let course: string = 'TypeScript'; // string
+let isPublished: boolean = true; // boolean
+
+let level; // any
+level = 1;
+level = 'mahdi'
+
+// with out type annotation
+let aSale = 123_456_789; // number
+let aCourse = 'TypeScript'; // string
+let aIsPublished = true; // boolean
+
+let numbers: number[] = [1, 3, 5]; // array
+let numbers2 = [1, 3, 5, '7']; // any array
+
+let numbers3: [number, string] = [1, 'mahdi']; // exactly two element with type
+
