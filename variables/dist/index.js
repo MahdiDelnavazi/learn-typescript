03422f54bc0de31676c6d491dea9140dce09c93f
MahdiDelnavazi <delnavazi.mahdi@gmail.com>
2022-09-19 16:12:59 +0430

    work on variables

---
+++ variables/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let sale = 123456789;
+let course = 'TypeScript';
+let isPublished = true;
+let level;
+level = 1;
+level = 'mahdi';
+let aSale = 123456789;
+let aCourse = 'TypeScript';
+let aIsPublished = true;
+let numbers = [1, 3, 5];
+let numbers2 = [1, 3, 5, '7'];
+let numbers3 = [1, 'mahdi'];
+//# sourceMappingURL=index.js.map
--- variables/src/index.ts
+++ variables/src/index.ts
@@ -13,6 +13,7 @@ let aIsPublished = true; // boolean
 
 let numbers: number[] = [1, 3, 5]; // array
 let numbers2 = [1, 3, 5, '7']; // any array
-
 let numbers3: [number, string] = [1, 'mahdi']; // exactly two element with type
 
+enum size { small = 1, medium, large } // enum small = 1 , medium = 2 , large = 3
+enum size2 { small = 's', medium = 'm', large = 'l' }
