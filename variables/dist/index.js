95479ad1801251fd46a8d94b49d69f4a11d2e26f
MahdiDelnavazi <delnavazi.mahdi@gmail.com>
2022-09-20 10:57:46 +0430

    work on variables

--- variables/dist/index.js
+++ variables/dist/index.js
@@ -11,4 +11,30 @@ let aIsPublished = true;
 let numbers = [1, 3, 5];
 let numbers2 = [1, 3, 5, '7'];
 let numbers3 = [1, 'mahdi'];
+var size;
+(function (size) {
+    size[size["small"] = 1] = "small";
+    size[size["medium"] = 2] = "medium";
+    size[size["large"] = 3] = "large";
+})(size || (size = {}));
+;
+var size2;
+(function (size2) {
+    size2["small"] = "s";
+    size2["medium"] = "m";
+    size2["large"] = "l";
+})(size2 || (size2 = {}));
+;
+;
+function calculateTax(income) {
+    return 0;
+}
+function calculateTax2(income) {
+    return 0;
+}
+function calculateTax3(income) {
+}
+function calculateTax4(income) {
+}
+console.log(size.large);
 //# sourceMappingURL=index.js.map
--- variables/src/index.ts
+++ variables/src/index.ts
@@ -4,7 +4,7 @@ let isPublished: boolean = true; // boolean
 
 let level; // any
 level = 1;
-level = 'mahdi'
+level = 'mahdi';
 
 // with out type annotation
 let aSale = 123_456_789; // number
@@ -15,5 +15,23 @@ let numbers: number[] = [1, 3, 5]; // array
 let numbers2 = [1, 3, 5, '7']; // any array
 let numbers3: [number, string] = [1, 'mahdi']; // exactly two element with type
 
-enum size { small = 1, medium, large } // enum small = 1 , medium = 2 , large = 3
-enum size2 { small = 's', medium = 'm', large = 'l' }
+enum size { small = 1, medium, large }; // enum small = 1 , medium = 2 , large = 3
+enum size2 { small = 's', medium = 'm', large = 'l' };
+const enum size3 { small = 's', medium = 'm', large = 'l' }; // const enum make compiler generate more optimize code
+
+function calculateTax(income: number) { // without annotation for return type
+    return 0;
+}
+
+function calculateTax2(income: number): number { // with annotation for return type
+    return 0;
+}
+
+function calculateTax3(income: number): void { // void means this function don`t return anything
+}
+
+function calculateTax4(income?: number): void { // if we write "?" after parameter, thats goes to optional parameter to pas
+}
+
+
+console.log(size.large);
