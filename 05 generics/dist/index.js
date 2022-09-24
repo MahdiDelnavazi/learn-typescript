951c6d977b8d4e4b68670816f7f5c4d223acaa08
mahdidelnavazi <delnavazi.mahdi@gmail.com>
2022-09-24 15:31:21 +0300

    add generic array

--- 05 generics/dist/index.js
+++ 05 generics/dist/index.js
@@ -7,4 +7,11 @@ class KeyValuePair {
 }
 let pair = new KeyValuePair('1', '1');
 let pair2 = new KeyValuePair(1, '1');
+class ArrayUtils {
+    static wrapInArray(value) {
+        return [value];
+    }
+}
+let numbers = ArrayUtils.wrapInArray(1);
+let strings = ArrayUtils.wrapInArray('1');
 //# sourceMappingURL=index.js.map
