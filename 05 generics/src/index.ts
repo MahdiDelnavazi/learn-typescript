482ad7aee0b1b984bb7ab4ac749e16244672c8f3
mahdidelnavazi <delnavazi.mahdi@gmail.com>
2022-09-24 15:29:56 +0300

    add generic array

--- 05 generics/src/index.ts
+++ 05 generics/src/index.ts
@@ -4,3 +4,15 @@ class KeyValuePair<T>{
 
 let pair = new KeyValuePair('1', '1')
 let pair2 = new KeyValuePair(1, '1')
+
+// generic array
+class ArrayUtils {
+    static wrapInArray<T>(value: T){
+        return[value];
+    }
+}
+
+let numbers = ArrayUtils.wrapInArray(1);
+let strings = ArrayUtils.wrapInArray('1');
+
+//add something
