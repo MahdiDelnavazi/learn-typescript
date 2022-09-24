3e2b6407db132db812a967a85d8f86e985d4abff
mahdidelnavazi <delnavazi.mahdi@gmail.com>
2022-09-24 16:39:16 +0300

    add generic interface

--- 05 generics/src/index.ts
+++ 05 generics/src/index.ts
@@ -1,3 +1,4 @@
+// generic class
 class KeyValuePair<T>{
     constructor(public key: T, public value: string) { }
 }
@@ -15,4 +16,28 @@ class ArrayUtils {
 let numbers = ArrayUtils.wrapInArray(1);
 let strings = ArrayUtils.wrapInArray('1');
 
-//add something
+//generic interface
+// http://myWeb.com/users
+// http://myWeb.com/products
+interface Result<T>{
+    data: T | null,
+    error: string | null
+}
+
+function fetch <T> (url: string): Result<T> {
+    return {data:null , error: null};
+}
+
+interface User {
+    userName: string;
+}
+
+interface Product {
+    title: string;
+}
+
+let resultUser = fetch <User>('user');
+resultUser.data?.userName
+
+let resultProduct = fetch <Product>('user');
+resultProduct.data?.title
