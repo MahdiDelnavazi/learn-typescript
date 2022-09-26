d52b6ed7e9ab73f6e33327f5360eab5d82d6f356
mahdidelnavazi <delnavazi.mahdi@gmail.com>
2022-09-26 14:27:42 +0300

    add sample decorator

--- 06 decorators/src/index.ts
+++ 06 decorators/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world');
+// decorators for change how class and members behave
+// its like extend
+function Component (constructor: Function){
+    console.log('component decorator called');
+    constructor.prototype.uniqueId = Date.now();
+}
+
+@Component
+class ProfileComponent {
+}
+
+let profile = new ProfileComponent();
