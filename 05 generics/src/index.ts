18bebc703e96af51c42076c1f00601ee8f81a7d4
MahdiDelnavazi <delnavazi.mahdi@gmail.com>
2022-09-28 10:13:01 +0330

    fix some codes

--- 05 generics/src/index.ts
+++ 05 generics/src/index.ts
@@ -2,5 +2,7 @@ class KeyValuePair<T>{
     constructor(public key: T, public value: string) { }
 }
 
-let pair = new KeyValuePair('1', '1')
-let pair2 = new KeyValuePair(1, '1')
+let pairNumber = new KeyValuePair('1', '1');
+pairNumber.key.split;
+let pairString = new KeyValuePair(1, '1');
+pairString.key.toExponential;
