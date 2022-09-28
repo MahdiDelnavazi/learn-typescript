74cecb34187919210297884ad14ddfb334cc3cda
MahdiDelnavazi <delnavazi.mahdi@gmail.com>
2022-09-28 12:31:56 +0330

    add post req handler to section 07

--- 07 node with typeScript/index.ts
+++ 07 node with typeScript/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import reminderRouter from './router/reminders';
 
 const app = express();
+
+app.use(express.json())
 app.use('/reminders', reminderRouter)
 
 app.get('/', (req, res) => {
--- 07 node with typeScript/router/reminders.ts
+++ 07 node with typeScript/router/reminders.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express'
+import CreateReminderDto from '../dtos/create-reminder';
 const router = Router();
 
 router.get('/', (req, res) => {
     res.send('List of reminders')
+})
+
+
+
+router.post('/', (req, res) => {
+    const { title } = req.body as CreateReminderDto;
+    res.json(title);
+
 })
 
 export default router;
